b01add02f0697cebc176bf6f7603c4e25eb399ce
Avrey Brown <avreyjbrown@yahoo.com>
2019-11-19 17:11:37 -0800

    iThinkImDone

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # lab-27
+
+## React Testing and Deployment
+
+### Author: Avrey Brown
+
+#### Links and Resources
+
+- https://dreamy-lamarr-b495f3.netlify.com/
+- aws wont let me use thier service at the moment
+### Modules
+
+#### 'footer.js'
+
+#### 'header.js'
+
+#### 'index.js'
+
+#### 'app.js'
+
+##### Exported Values and Methods
---
+++ build/precache-manifest.b1011bbb8fce186d11ab93afc25b6048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5add8c19aa2e0370ae85dd4fc7a6b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a0971cd84afd32ea3de",
+    "url": "/static/css/main.d90503d3.chunk.css"
+  },
+  {
+    "revision": "8563d58186bc11ae4e60",
+    "url": "/static/js/2.6974d759.chunk.js"
+  },
+  {
+    "revision": "1a0971cd84afd32ea3de",
+    "url": "/static/js/main.1243be77.chunk.js"
+  },
+  {
+    "revision": "ccf13770f5cc82075f39",
+    "url": "/static/js/runtime-main.11de49cd.js"
+  }
+]);
